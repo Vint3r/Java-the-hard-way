b8ec20d79d89dabc09dd0060df36cf3c9c3fdd7a
David Norris <Norris.David.Dev@gmail.com>
2019-08-20 10:14:30 -0600

    Changed lines 3 and 4

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main(String[] args) {
-    System.out.println("I am determined to learn how to code.");
-    System.out.println("Today's date is");
+    System.out.println("David is determined to learn how to code.");
+    System.out.println("Today's date is Tuesday, August 20th");
   }
 }
