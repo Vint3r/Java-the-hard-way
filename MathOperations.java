c4eb9876bb966657b83de77d46a27ad803622be1
David Norris <Norris.David.Dev@gmail.com>
2019-08-20 13:56:36 -0600

    Lots of numbers

---
+++ MathOperations.java
@@ -0,0 +1,43 @@
+public class MathOperations {
+  public static void main(String[] args) {
+    int a, b, c, d, e, f, g, j;
+    double x, y, z, i, h;
+    String one, two, both;
+
+    a = 10;
+    b = 27;
+    System.out.println("a is " + a + ", b is " + b);
+
+    c = a + b;
+    System.out.println ("a+b is " + c);
+    d = a - b;
+    System.out.println("a-b is " + d);
+    e = a + b * 3;
+    System.out.println("a+b*3 is " + e);
+    f = b / 2;
+    System.out.println("b/2 is " + f);
+    g = b % 10;
+    System.out.println("b%10 is " + g);
+
+    x = 1.1;
+    System.out.println("\nx is " + x);
+    y = x*x;
+    System.out.println("x*x is " + y);
+    z = b / 2;
+    System.out.println("b/2 is " + z);
+    System.out.println();
+
+    one = "dog";
+    two = "house";
+    both = one + two;
+    System.out.println(both);
+
+    i = z * a;
+    h = i / e + g;
+    j = b - f;
+    System.out.println("z*a is " + i);
+    System.out.println("i/e+g is " + h);
+    System.out.println("b-f is " + j);
+
+  }
+}
